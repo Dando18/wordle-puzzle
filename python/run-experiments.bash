052e5ae792a773143f683e0971562fbf1a01e40a
Daniel Nichols <dando18studios@gmail.com>
2022-01-20 10:31:59 -0500

    update experiments

--- python/run-experiments.bash
+++ python/run-experiments.bash
@@ -5,7 +5,7 @@ SEED="42"
 FIRST_GUESS="--smart-first-guess"
 ITER="5000"
 LOW_ITER="1000"
-MP="--multiprocessing 4"
+MP="--multiprocessing"
 
 printf "\n\nRandom\n======\n"
 $EXEC --seed $SEED $FIRST_GUESS -n $ITER $MP -p random
@@ -17,7 +17,7 @@ printf "\n\nProbabilistic Greedy\n====================\n"
 $EXEC --seed $SEED $FIRST_GUESS -n $ITER $MP -p prob_greedy
 
 printf "\n\nGenetic Algorithm\n=================\n"
-$EXEC --seed $SEED $FIRST_GUESS -n $ITER $MP -p genetic
+$EXEC --seed $SEED $FIRST_GUESS -n $LOW_ITER $MP -p genetic
 
 printf "\n\nMinimax\n=======\n"
-$EXEC --seed $SEED $FIRST_GUESS -n $ITER $MP -p minimax
+$EXEC --seed $SEED $FIRST_GUESS -n $LOw_ITER $MP -p minimax
